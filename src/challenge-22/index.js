a5f95428333b509a16498a21c540ac75f1b0a672
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-07-28 11:03:43 -0600

    :sparkles: Add challenge 22 in js and ts

---
+++ src/challenge-22/index.js
@@ -0,0 +1,27 @@
+function compile(code) {
+  let counter = 0
+  let stack = -1
+
+  for (let i = 0; i < code.length; i++) {
+    const instruction = code[i]
+
+    if (instruction === '+') counter++
+    else if (instruction === '-') counter--
+    else if (instruction === '*') counter *= 2
+    else if (instruction === '%') stack = i
+    else if (instruction === '<') {
+      if (stack !== -1) {
+        i = stack
+        stack = -1
+      }
+    } else if (instruction === '¿') {
+      if (counter <= 0) {
+        i = code.indexOf('?', i)
+      }
+    }
+  }
+
+  return counter
+}
+
+module.exports = compile
