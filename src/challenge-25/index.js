9fe56c3787fdfb687da54aa5bda40f2abb8558b2
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-07-28 11:04:16 -0600

    :sparkles: Add challenge 25 in js and ts

---
+++ src/challenge-25/index.js
@@ -0,0 +1,30 @@
+function travelDistance(map) {
+  const roadmapArr = map.split('\n')
+  const roadmap = roadmapArr.join('')
+  const cols = roadmapArr[0].length
+  const santaPos = roadmap.indexOf('S')
+
+  let gift = 1
+  let movements = 0
+  let santaCol = santaPos % cols
+  let santaRow = Math.floor(santaPos / cols)
+
+  const numbers = roadmap.replace(/\.|S/g, '')
+
+  for (const _number of numbers) {
+    const giftPos = roadmap.indexOf(`${gift}`)
+    const giftCol = giftPos % cols
+    const giftRow = Math.floor(giftPos / cols)
+
+    movements += Math.abs(santaRow - giftRow) + Math.abs(santaCol - giftCol)
+
+    santaCol = giftCol
+    santaRow = giftRow
+
+    gift++
+  }
+
+  return movements
+}
+
+module.exports = travelDistance
